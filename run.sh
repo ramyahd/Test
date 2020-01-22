521c8590b1b35edf26d3fd4a92f41c0fb63d0cb1
Komaravelli Manikanta (Digital) <KO20094862@wipro.com>
2020-01-22 17:02:23 +0530

    Jenkinsfile edited to create a new job

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 curl -X POST http://${username}@${url}/job/${job}/build 
 var=$(curl -XGET http://${url}/checkJobName?value=${NewJob} --user ${username})
-echo $var
+var1=<div/>
+if [ "$var" = "$var1" ]; then
+curl -s -XPOST http://${url}/createItem?name=${NewJob} --data-binary @config.xml -H "Content-Type:text/xml" --user ${username}
+fi
