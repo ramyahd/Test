c63c1fe9ff32838b092b1cbea061709bc494ae1e
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-01-24 12:31:13 +0530

    Delete run.sh

--- run.sh
+++
@@ -1,6 +0,0 @@
-curl -X POST http://${username}@${url}/job/${job}/build 
-var=$(curl -XGET http://${url}/checkJobName?value=${NewJob} --user ${username})
-var1="<div/>"
-if [ "$var" = "$var1" ]; then
-curl -s -XPOST http://${url}/createItem?name=${NewJob} --data-binary @config.xml -H "Content-Type:text/xml" --user ${username}
-fi
